ab8e7060ddce24f3d2ecb73cfa7affef09dfc137
Kevin <68694478+Mbuzinous@users.noreply.github.com>
2025-02-06 13:10:42 +0100

    Student class. Public properties stringName, int BirthMonth and int BirthYear. That can privately be set by the constructor.

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        public string Name { get; private set; }
+        public int BirthMonth { get; private set; }
+
+        public int BirthYear { get; private set; }
+
+        public Student(string name, int birthMonth, int birthYear)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            BirthYear = birthYear;
+        }
+
+
+    }
+}
